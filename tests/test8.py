6f05e8ef31d1175d604f697daadf3b52e1e31f5a
MateoPissarello <mateopissa@gmail.com>
2024-09-12 20:45:23 -0500

    final

--- tests/test8.py
+++ tests/test8.py
@@ -1,8 +1,1 @@
-def suma():
-cuantos_sum=int(input("Escribe cuantos números quieres sumar: "))
-resultado_sum=[]
-for i in range(cuantos_sum):
-    num_sum=int(input("Ingrese el número de la suma: "))
-    resultado_sum.append(str(num_sum))
-resultado_sum=sum(str(resultado_sum))
-return "El resultado de la suma es: " + str(suma)
+¬
